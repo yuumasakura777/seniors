a1ab18a6733a4a8144f395151517152b80cc5c1b
Tyuusaku777 <mikagamit777@gmail.com>
2019-09-01 20:36:35 +0900

    alter relationship controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,11 +6,11 @@ class RelationshipsController < ApplicationController
     follow=current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
     #マッチングしているかしていないかでメールの条件分岐をする
-    @f_user=User.find(params[:user_id])
+    @mail_user=User.find(params[:user_id])
     if follow_matcher(params[:user_id]).blank?
-      NotificationMailer.send_follower_users(current_user, @f_user.name, @f_user.email).deliver
+      NotificationMailer.send_follower_users(current_user, @mail_user.name, @mail_user.email).deliver
     else
-      NotificationMailer.send_matcher_users(current_user, @f_user.name, @f_user.email).deliver
+      NotificationMailer.send_matcher_users(current_user, @mail_user.name, @mail_user.email).deliver
     end
     flash[:success]="#{@user.name}さんをフォローしました。"
     redirect_to user_path(params[:user_id])
